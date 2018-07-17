89465fd87c7a8e051a5e6ca4903357185292fd58
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2018-07-17 18:50:12 +0200

    Update index.js

--- .gitignore/.gitignore/index.js
+++ .gitignore/.gitignore/index.js
@@ -28,27 +28,22 @@ bot.on('message' , message => {
             .addField("- /test", `${aide_1_4}`)
             .addField("- /futur", `${aide_1_5}`)
         message.channel.send({embed: aide_1});
-        console.log("help effectué")
     }      
 
     if (message.content === prefix + "version"){
         message.channel.sendMessage(" *Bip Bop* \n Voici mes détails \n -numéro de version 1.0 \n Pour voir tout les ajouts vas voir sur le site (lien du site grâce à la commande /site) *Bip Bop*"); // voir pour ajouter autre chose
-        console.log("bot effectué")
     }
 
     if (message.content === prefix + "site"){
         message.channel.sendMessage("La site arrive bientôt !!"); //ajouter le lien du site du bot
-        console.log("site effectué")
     }
     
     if (message.content === prefix + "test"){
         message.channel.sendMessage("Test chat validé");
-        console.log("Test effectué")
     }
 
     if (message.content === prefix + "ping"){
         message.reply("HMMMM... T'as " + `${message.createdTimestamp - Date.now()}` + "MS de ping... C'EST A CHIEZ GROS CON !! AVEC TA CO EN CARTON!!");
-        console.log("ping effectué")
     }   
      
     if (message.content === prefix + "futur"){
@@ -62,7 +57,6 @@ bot.on('message' , message => {
                 .addField("- de nouvelles commandes", `${futur_2}`)
                 .addField("N'hésitez pas à donner des idées pour le bot !", `${futur_fin}`)
             message.channel.send({embed: futur});
-        console.log("futur effectué")
     }
 
     if (message.content === prefix + "help2"){
@@ -79,45 +73,55 @@ bot.on('message' , message => {
             .addField("- /D", `${aide_2_1}`)
             .addField("- /bafouille", `${aide_2_1}`)
             .addField("- /zorana", `${aide_2_1}`)
+            .addField("- /rasoir", `${aide_2_1}`)
+            .addField("- /tatayoyo", `${aide_2_1}`)
+            .addField("- /kermit", `${aide_2_1}`)
         message.channel.send({embed: aide_2_2});
-        console.log("help2 effectué")
     }
 
     if (message.content === prefix + "saucisse"){
         message.channel.send("OPÉRATION SAUCISSE, OPÉRATION CUISSON-SSON !! \nOPÉRATION SAUCISSE, OPÉRATION CUISSON-SSON !! \nOPÉRATION SAUCISSE, OPÉRATION CUISSON-SSON !! \nOPÉRATION SAUCISSE, OPÉRATION CUISSON-SSON !!");
-        console.log("saucisse effectué")
     }
 
     if (message.content === prefix + "69"){
         message.channel.send("OPÉRATION PÉNIS, OPÉRATION FÉLATION-TION !!\nOPÉRATION PÉNIS, OPÉRATION FÉLATION-TION !!\nOPÉRATION PÉNIS, OPÉRATION FÉLATION-TION !!\nOPÉRATION PÉNIS, OPÉRATION FÉLATION-TION !!\n" );
-        console.log("69 effectué")
     }
 
     if (message.content === prefix + "gaetan"){
         message.channel.sendMessage("Gaétan dans environ 5-6 ans ==> https://www.youtube.com/watch?v=KZb3GUOWX8I");
-        console.log("gaetan effectué")
     }
 
     if (message.content === prefix + "love"){
         message.channel.sendMessage("WHAT IS LOVE !? Tel est la question ! https://www.youtube.com/watch?v=dAIpjgvhKsA ");
-        console.log("WiL effectué")
     }
 
     
     if (message.content === prefix + "D"){
         message.channel.sendMessage("D, la réponse D");
-        console.log("D effectué")
     }
     
     
     if (message.content === prefix + "bafouille"){
         message.channel.sendMessage("C'est votre ultime bafouille Gui ?");
-        console.log("bafouille effectué")
     }
     
     
     if (message.content === prefix + "zorana"){
         message.channel.sendMessage("BONJOUR! Je suis le pésident à ZORANA! \nSi je suis là, c\'est parce qu'est en SPÉCIAL! ELLE A DES BUBULLES !! ");
-        console.log("eau effectué")
     }
+   
+    if (message.content === prefix + "rasoir"){
+        message.channel.sendMessage("GILLETTE !!!! THE BEST MAN CAN GET!!! :microphone: :musical_score: :microphone: :musical_score: https://www.youtube.com/watch?v=9fWxCIi5PIw");
+    }
+
+    if (message.content === prefix + "tatayoyo"){
+        for (var iter = 0; iter < 10; iter++)
+        message.channel.sendMessage("@everyone TATAAAAAAAA YOYOOOOO");
+    }
+
+    if (message.content === prefix + "kermit"){
+        for (var iter = 0; iter < 20; iter++)
+        message.channel.sendMessage("@KermitOfficiel THE BEST MAN CAN GET !!!!!");
+    }
+    
 });
