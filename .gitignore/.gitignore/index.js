44e1b25e1f5c92f7fdea8000e96b131dbeeace72
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2018-08-26 16:22:38 +0200

    Update index.js

--- .gitignore/.gitignore/index.js
+++ .gitignore/.gitignore/index.js
@@ -1,7 +1,15 @@
 const Discord =  require('discord.js');
 const bot = new Discord.Client();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [], xp: [] }).write()
 
 var prefix = ("/");
+var prefix2 = ("@");
 
 bot.on('ready', function() {
     bot.user.setGame ("command: /help ou /help2")
@@ -13,57 +21,76 @@ bot.login(process.env.TOKEN);
 bot.on('message' , message => {
     
     if (message.content === prefix + "help"){
-    var aide_1_1 = ("Pour voir à quelle version je suis !")
-    var aide_1_2 = ("Pour avoir l\' URL du site *coming soon*")  
-    var aide_1_3 = ("Pour obtenir ton ping") 
-    var aide_1_4 = ("Pour voir si le bot marche bien et qu\'il est pas tout casser")
-    var aide_1_5 = ("Pour les idées et ajouts futurs du bot !")
+    var aide_1_1 = ("Pour voir à quelle version je suis !") 
+    var aide_1_2 = ("Pour obtenir ton ping") 
+    var aide_1_3 = ("Pour voir si le bot marche bien et qu\'il est pas tout casser")
+    var aide_1_4 = ("Pour voir les règles du système d'XP")
+    var aide_1_5 = ("Pour voir le tableau d'XP")
+    var aide_1_6 = ("Pour les idées et ajouts futurs du bot !")
         
         var aide_1 = new Discord.RichEmbed()
             .setTitle("Liste des commandes informatives")
             .setColor(`#DB1C1C`)
             .addField("- /version", `${aide_1_1}`)
-            .addField("- /site", `${aide_1_2}`)
-            .addField("- /ping", `${aide_1_3}`)
-            .addField("- /test", `${aide_1_4}`)
-            .addField("- /futur", `${aide_1_5}`)
+            .addField("- /ping", `${aide_1_2}`)
+            .addField("- /test", `${aide_1_3}`)
+            .addField("- /rxp", `${aide_1_4}`)
+            .addField("- /xp", `${aide_1_5}`)
+            .addField("- /futur", `${aide_1_6}`)
         message.channel.send({embed: aide_1});
     }      
 
     if (message.content === prefix + "version"){
-        message.channel.sendMessage(" *Bip Bop* \n Voici mes détails \n -numéro de version: 1.1.3 \n Pour voir tout les ajouts vas voir sur le site (lien du site grâce à la commande /site) *Bip Bop*"); // voir pour ajouter autre chose
+        var version1_1 = ("**Version 2.0**") 
+        var version1_2 = ("Le système d'XP est enfin là !! *vas vite fait voir les règles quand même en tapant /rxp* \n Des améliorations arrivent du système bientôt !*pour plus d'information ==> /futur* ")
+            
+            var version1 = new Discord.RichEmbed()
+                .setTitle("NotePatch")
+                .setColor(`#DB1C1C`)
+                .addField("Version ZululBot", `${version1_1}`)
+                .addField("Ajouts V2.0", `${version1_2}`)
+            message.channel.send({embed: version1});
     }
-
-    if (message.content === prefix + "site"){
-        message.channel.sendMessage("La site arrive bientôt !!"); //ajouter le lien du site du bot
-    }
-    
+  
     if (message.content === prefix + "test"){
         message.channel.sendMessage("Test chat validé");
     }
 
     if (message.content === prefix + "ping"){
-        message.reply("HMMMM... T'as " + `${message.createdTimestamp - Date.now()}` + "MS de ping... C'EST A CHIEZ GROS CON !! AVEC TA CO EN CARTON!!");
-    }   
+        message.reply("PONG !! Latence actuelle: " + `${message.createdTimestamp - Date.now()}` + " ms");
+    }
+    
+    if (message.content === prefix + "rxp"){
+        var rxp1 = ("Il suffit d'écrire un message pour obtenir 1 point") 
+        var rxp2 = ("Tous message considéré comme du spam ou spam sera reconnu comme triche ou anti-jeu et sera puni allant d'un rappel à un mute temporaire, un ban temporaire ou même d'une remise à zéro de son compteur d'XP!")
+            
+            var rxp0 = new Discord.RichEmbed()
+                .setTitle("Règle du système d'XP")
+                .setColor(`#DB1C1C`)
+                .addField("Gagner de l'xp c'est facile ! ", `${rxp1}`)
+                .addField("MAIS ATTENTION ! IL Y A UNE REGLE !", `${rxp2}`)
+            message.channel.send({embed: rxp0});
+    }
      
     if (message.content === prefix + "futur"){
-        var futur_1 = ("un peu comme à la mee6 quoi")
-        var futur_2 = ("des commandes toujours plus folles et débilles")
-        var futur_3 = ("un peu à la rythm maison")
-        var futur_fin = ("pour le futur et le bien être du serveur !")         
+        var zbv3 = ("Amélioration du système d'xp avec des levels et des rôles ainsi qu'un cooldown pour éviter tout problème de spam ")
+        var zbv4 = ("Une commande toute simple qui permet de mentionner entre 5 et 10 fois la personne demandée")
+        var zbv5 = ("Ajout d'un système audio *Un rythm maison on va dire* ")
+        var futur_fin = ("Pour le futur et le bien être du serveur !")         
+            
             var futur = new Discord.RichEmbed()
                 .setTitle("Liste des futur ajouts au bot !")
                 .setColor(`#DB1C1C`)
-                .addField("- un système d\'XP et de level", `${futur_1}`)
-                .addField("- un système musical", `${futur_3}`)
-                .addField("- de nouvelles commandes", `${futur_2}`)
+                .addField("Mise à jour ZulutBot V3.0 :", `${zbv3}`)
+                .addField("Mise à jour ZulutBot V4.0 :", `${zbv4}`)
+                .addField("Mise à jour ZulutBot V5.0 :", `${zbv5}`)
                 .addField("N'hésitez pas à donner des idées pour le bot !", `${futur_fin}`)
             message.channel.send({embed: futur});
     }
 
     if (message.content === prefix + "help2"){
         var aide_2_1 = ("essaie la et tu verra ! :wink: ")
-        
+                
         var aide_2_2 = new Discord.RichEmbed()
             .setTitle("Liste des commandes secondaires")
             .setColor(`#DB1C1C`)
@@ -71,8 +98,7 @@ bot.on('message' , message => {
             .addField("- /love", `${aide_2_1}`)
             .addField("- /gaetan", `${aide_2_1}`)
             .addField("- /69", `${aide_2_1}`)
-            .addField("- /saucisse", `${aide_2_1}`)
-            .addField("- /D", `${aide_2_1}`)
+            .addField("- /repd", `${aide_2_1}`)
             .addField("- /bafouille", `${aide_2_1}`)
             .addField("- /zorana", `${aide_2_1}`)
             .addField("- /rasoir", `${aide_2_1}`)
@@ -96,25 +122,48 @@ bot.on('message' , message => {
         message.channel.sendMessage("WHAT IS LOVE !? Tel est la question ! https://www.youtube.com/watch?v=dAIpjgvhKsA ");
     }
     
-    if (message.content === prefix + "D"){
+    if (message.content === prefix + "repd"){
         message.channel.sendMessage("D, la réponse D");
     }
         
     if (message.content === prefix + "bafouille"){
         message.channel.sendMessage("C'est votre ultime bafouille Gui ?");
     }
-     
+        
     if (message.content === prefix + "zorana"){
         message.channel.sendMessage("BONJOUR! Je suis le pésident à ZORANA! \nSi je suis là, c\'est parce qu'est en SPÉCIAL! ELLE A DES BUBULLES !! ");
     }
-   
+
     if (message.content === prefix + "rasoir"){
-        message.channel.sendMessage("GILLETTE !!!! THE BEST MAN CAN GET!!! :microphone: :musical_score: :microphone: :musical_score: \n https://www.youtube.com/watch?v=9fWxCIi5PIw");
+        message.channel.sendMessage("GILLETTE !!!! THE BEST MAN CAN GET!!! :microphone: :musical_score: :microphone: :musical_score: https://www.youtube.com/watch?v=9fWxCIi5PIw");
     }
 
     if (message.content === prefix + "tatayoyo"){
-        for (var iter = 0; iter < 10; iter++)
+        for (var boucle = 0; boucle < 10; boucle++)
         message.channel.sendMessage("@everyone TATAAAAAAAA YOYOOOOO");
     }
-    
+
+    var perso = message.author.username;
+
+    if(message.author.bot)return;
+
+    if(!db.get("xp").find({user: perso}).value()){
+        db.get("xp").push({user: perso,xp: 1}).write();
+        
+    }else{
+        var userxpdb = db.get("xp").filter({user: perso}).find('xp').value();
+        console.log(userxpdb)
+        var userxp = Object.values(userxpdb)
+        db.get("xp").find({user: perso}).assign({user: perso, xp: userxp[1] +=1}).write();
+
+    if (message.content === prefix + "xp"){
+        var xp = db.get("xp").filter({user: perso}).find('xp').value()
+        var xpfinal = Object.values(xp);
+        var xp_embed = new Discord.RichEmbed()
+            .setTitle(`Stat des XP de ${message.author.username}`)
+            .setColor(`#F4D03F`)
+            .addField("Niveau")
+            .addField("XP total:", `${xpfinal[1]} points d'expériences`)
+        message.channel.send({embed: xp_embed});
+    }} 
 });
