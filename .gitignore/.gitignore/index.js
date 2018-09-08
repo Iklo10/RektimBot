9a477d70583bbb3cfc7b61ff66d61c1154a80e0f
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2018-09-08 20:56:46 +0200

    Update index.js

--- .gitignore/.gitignore/index.js
+++ .gitignore/.gitignore/index.js
@@ -162,7 +162,7 @@ bot.on('message' , message => {
         var xp_embed = new Discord.RichEmbed()
             .setTitle(`Stat des XP de ${message.author.username}`)
             .setColor(`#F4D03F`)
-            .addField("XP total:", `${xpfinal[1]} points d\'expériences`)
+            .addField(`XP total: ${xpfinal[1]} points `)
         message.channel.send({embed: xp_embed});
     }}
 
