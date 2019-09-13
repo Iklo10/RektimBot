27ddff9ce32cc2a14687905314ae2c93bcd74dd7
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:24:24 +0200

    Update love.js

--- .gitignore/.gitignore/commands/love.js
+++ .gitignore/.gitignore/commands/love.js
@@ -1,3 +1,3 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send("WHAT IS LOVE !? Tel est la question ! ", {files:['https://www.youtube.com/watch?v=dAIpjgvhKsA']});
+    msg.channel.send("WHAT IS LOVE !? Tel est la question ! https://www.youtube.com/watch?v=dAIpjgvhKsA");
 }
