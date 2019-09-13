7eb9b5ec526de5c159d5241b5618841ed988bc8b
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:16:58 +0200

    Update help.js

--- .gitignore/.gitignore/commands/help.js
+++ .gitignore/.gitignore/commands/help.js
@@ -2,7 +2,6 @@ exports.run = (client, msg, args) => {
     const Discord = require("discord.js");
 
     const aide1 = ("Pour voir ma version et mes ajouts !") 
-    const aide2 = ("Pour le concours de bite du nombre")
     const aide3 = ("Pour avoir le lien du site Rektim **Coming Soon** ")
     const aide4 = ("Pour voir la seconde partie des commandes")
         
@@ -10,7 +9,6 @@ exports.run = (client, msg, args) => {
             .setTitle("Liste des commandes informatives")
             .setColor(`#87CEEB`)
             .addField("**- /version**", `${aide1}`)
-            .addField("**- /random**", `${aide2}`)
             .addField("**- /site**", `${aide3}`)
             .addField("**- /help2**", `${aide4}`)
         msg.channel.send({embed: aide});
