d9a255724d1482d7df8f804e6a8b6549a90b7cdf
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:01:11 +0200

    Create tatayoyo.js

---
+++ .gitignore/.gitignore/commands/tatayoyo.js
@@ -0,0 +1,4 @@
+exports.run = (client, msg, args) => {
+    for (var boucle = 0; boucle < 5; boucle++)
+    msg.channel.send("@everyone TATAAAAAAAA YOYOOOOO :microphone: :musical_score: :microphone: :musical_score: ");
+}
