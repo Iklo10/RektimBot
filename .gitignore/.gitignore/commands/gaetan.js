615650e921a0888f7b8e1e1399b00d250fde400c
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:23:55 +0200

    Update gaetan.js

--- .gitignore/.gitignore/commands/gaetan.js
+++ .gitignore/.gitignore/commands/gaetan.js
@@ -1,3 +1,3 @@
 exports.run = (client, msg, args) => {
-    msg.channel.send("Gaétan dans environ 5-6 ans ==>", {files:['https://www.youtube.com/watch?v=KZb3GUOWX8I']});
+    msg.channel.send("Gaétan dans environ 5-6 ans ==> https://www.youtube.com/watch?v=KZb3GUOWX8I");
 }
