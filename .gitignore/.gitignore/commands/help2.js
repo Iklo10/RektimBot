7f37ca6fb9630cbfaefef495b734679de4f916d5
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:04:08 +0200

    Create help2.js

---
+++ .gitignore/.gitignore/commands/help2.js
@@ -0,0 +1,17 @@
+exports.run = (client, msg, args) => {
+    const Discord =  require("discord.js");
+
+    const cmd_basic = ("Test la ! Et tu verra !") 
+    const tata_yoyo = ("Spam 5 fois la commande liée et mentionne tous le monde")
+    const rdm = ("Tire au hasard une image ou une phrase dans une banque de données maison")
+    const help2_fin = ("**Enjoy**")
+
+    const help2 = new Discord.RichEmbed()
+        .setTitle("Liste des commande secondaire !")
+        .setColor(`#87CEEB`)
+        .addField("**- /fortnite \n- /saucisse \n- /gaetan \n- /love \n- /repd \n- /bafouille \n- /zorana**", `${cmd_basic}`)
+        .addField("**- /tatayoyo**", `${tata_yoyo}`)
+        .addField("**- /wankul \n- /tg**", `${rdm}`)
+        .addField("**D'autres petites commandes débiles arriveront sûrement bientôt !**", `${help2_fin}`)
+    msg.channel.send({embed: help2});
+}
