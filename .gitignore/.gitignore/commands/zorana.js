642a81aecc882cebf7ca478f6dc37fd10213c1af
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 19:00:49 +0200

    Create zorana.js

---
+++ .gitignore/.gitignore/commands/zorana.js
@@ -0,0 +1,3 @@
+exports.run = (client, msg, args) => {
+    msg.channel.send("BONJOUR! Je suis le pésident à ZORANA! \nSi je suis là, c'est pour vous présenter mon eau. Parce que mon eau elle est SPÉCIAL! ELLE A DES BUBULLES !!", {files:['https://mediagamaniak.cdn.re/vidcap/palmashow-parodie-pub.mp4.jpg']});
+}
