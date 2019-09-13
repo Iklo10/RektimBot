d6d0fb716ef3cc86c02c8b329894430eff6b0ad5
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-13 18:59:31 +0200

    Create version.js

---
+++ .gitignore/.gitignore/commands/version.js
@@ -0,0 +1,15 @@
+exports.run = (client, msg, args) => {
+    const Discord = require("discord.js");
+
+    const version1 = ("**Version 2.0**") 
+    const version2 = ("Une refonte totale c'est faite ! Le bot est donc \n - plus beau ! \n - plus fort ! \n - plus rapide ! \n - plus performant ! \n - ET SURTOUT... Plus facile à lui ajouter des conneries et des fonctionnalités bien sympa ! \n **(les commandes restent les mêmes sauf pour /ping qui a été supprimer)**" )
+
+    const changelog = ("** https://github.com/Iklo10/RektimBot/wiki#version-20 **")
+    
+        const version = new Discord.RichEmbed()
+            .setColor(`#87CEEB`)
+            .addField("Version RektimBot", `${version1}`)
+            .addField("V2.0", `${version2}`)
+            .addField("Pour les changelogs version par version, c'est ici :", `${changelog}`)
+        msg.channel.send({embed: version});
+}
