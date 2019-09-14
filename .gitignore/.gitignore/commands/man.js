f45c2e0ba62c25e127253fc40c3721f95e08887c
Iklo10 <40717116+Iklo10@users.noreply.github.com>
2019-09-14 14:26:24 +0200

    Create man.js

---
+++ .gitignore/.gitignore/commands/man.js
@@ -0,0 +1,7 @@
+exports.run = (client, msg, args) => {
+    const Discord = require("discord.js")
+    
+    if (!args[0]) return msg.channel.send("Pour utiliser la commande: /man + le nom de superhéros que tu veux te donner");
+
+    msg.reply("tu est vraiment " + `${args.join('')}` + "man !!"); 
+}
